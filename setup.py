951750a2523663ea9f019f48d69db4eba20f473d
Assaf Urieli <assafurieli@gmail.com>
2021-06-02 15:56:32 +0200

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+from setuptools import setup, find_packages
+import sys
+
+setup(
+  name="DeepLabV3Plus-Pytorch",
+  version="0.0.1",
+  license="MIT",
+  url="https://github.com/VainF/DeepLabV3Plus-Pytorch",
+  description="DeepLabv3, DeepLabv3+ and pretrained weights on VOC & Cityscapes.",
+  packages=find_packages(),
+  install_requires=[
+    "torch>=1.8.0",
+    "torchvision>=0.9.0",
+    "numpy>=1.18.1,<1.19.0",
+    "Pillow>=8.2.0",
+    "tqdm>=4.54.1",
+    "scikit-learn>=0.23.2",
+    "matplotlib>=3.3.3",
+    "visdom>=0.1.8.9"
+  ],
+  zip_safe=False,
+)
